1a7a92f260a1fbea74adfe980acb4b7c854c9795
Dawid <dawid.migowski@gmail.com>
2022-11-13 21:34:09 +0100

    lab3 gateway

--- src/main/java/lab/lab1/repository/ContinentRepository.java
+++
@@ -1,9 +0,0 @@
-package lab.lab1.repository;
-
-import lab.lab1.entity.Continent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@org.springframework.stereotype.Repository
-public interface ContinentRepository extends JpaRepository<Continent, String> {
-
-}
